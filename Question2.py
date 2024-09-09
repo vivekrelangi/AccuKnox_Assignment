9ededd1b3abd2278dab26ee68f25afe7360a8955
vivekrelangi <84368615+vivekrelangi@users.noreply.github.com>
2024-09-09 14:30:14 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -1,4 +1,4 @@
-"""Answer : Yes, by default, Django signals run in the same thread as the caller. In the below code, both the caller (the create_user view) and the signal handler run in the same thread, as indicated by the same thread ID printed in the output."""
+"""Answer : Yes, by default, Django signals run in the same thread as the caller. To run the below code follow steps given in README.md file of the repo. In the below code, both the caller (the create_user view) and the signal handler run in the same thread, as indicated by the same thread ID printed in the output."""
 
 # signals.py
 from django.db.models.signals import post_save
